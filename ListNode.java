efaca77427fe1f162640c2713d041e676a4c8f3b
wind0926 <50622462+wind0926@users.noreply.github.com>
2019-08-17 23:00:04 +0800

    Add files via upload

---
+++ ListNode.java
@@ -0,0 +1,23 @@
+/*
+public class ListNode {
+    int val;
+    ListNode next = null;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}*/
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode prev=null;
+        ListNode curr=head;
+        ListNode next=null;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        return prev;
+    }
+}
